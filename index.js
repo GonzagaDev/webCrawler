3ef21eba9025d0564dc17be690ee023cb4723638
cristiano.gonzaga <cristianogonzaga.si@gmail.com>
2017-10-13 13:49:13 -0300

    Estruturação do projeto

--- index.js
+++ index.js
@@ -2,33 +2,30 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const hbs = require('express-hbs')
 const logger = require('morgan')
-const session = require('express-session')
 const path = require('path')
 const app = express()
+var porta = process.env.PORT || 8080;
 
 app.set('views', path.join(__dirname, 'src/views'))
 app.set('view engine', 'hbs')
-app.set('port', 8080)
+
+
 
 app.use(logger('dev'))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(session({ 
-	secret: 'D&@#*(DASKJHDKH!(#&(&@',
-	resave: false,
-	saveUninitialized: true,
-	cookie: { }
-}))
+app.use(bodyParser.urlencoded({extended: false}))
 app.engine('hbs', hbs.express4({
-	defaultLayout: path.join(app.get('views'), 'layouts/home.hbs'),
-	partialsDir  : path.join(app.get('views'), 'partials'),
-	layoutsDir   : path.join(app.get('views'), 'layouts'),
+    defaultLayout: path.join(app.get('views'), 'layouts/home.hbs'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    layoutDir: path.join(app.get('views'), 'layouts'),
 }))
 
 require('./src/routes')(app)
 
-app.listen(app.get('port'), () => {
-	console.log('Express has been started')
+
+app.listen(app.get(porta),()=>{
+    console.log('Serviço iniciado na porta'+porta);
 })
 
 module.exports = app
+
